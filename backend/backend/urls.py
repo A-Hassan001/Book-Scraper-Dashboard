71fc22f9a636a175c608e28fb4269bd6959a0342
A-Hassan001 <contact.abhassan@gmail.com>
2026-02-26 11:27:22 +0500

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,21 @@
+# from django.contrib import admin
+# from django.urls import path, include
+
+
+# urlpatterns = [
+#     path("admin/", admin.site.urls),
+#     path('api/', include('api.urls'))
+# ]
+
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
+def home(request):
+    return redirect('/api/dashboard/')  # adjust if needed
 
 urlpatterns = [
+    path('', home),                     # ✅ ROOT redirect (VERY IMPORTANT)
     path("admin/", admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
 ]
